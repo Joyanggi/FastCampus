dc04bda6e0d2c11a43f2ffb3c36c6b43b15a4c94
Joyanggi <jyg8033@gmail.com>
2023-04-12 11:10:34 +0900

    facade

--- src/main/java/com/company/design/Main.java
+++ src/main/java/com/company/design/Main.java
@@ -3,6 +3,10 @@ package com.company.design;
 import com.company.design.adapter.*;
 import com.company.design.aop.AopBrowser;
 import com.company.design.decorator.*;
+import com.company.design.facade.Ftp;
+import com.company.design.facade.Reader;
+import com.company.design.facade.SftpClient;
+import com.company.design.facade.Writer;
 import com.company.design.observer.Button;
 import com.company.design.observer.IButtonListener;
 import com.company.design.proxy.Browser;
@@ -12,22 +16,34 @@ import com.company.design.singleton.AClazz;
 import com.company.design.singleton.BClazz;
 import com.company.design.singleton.SocketClient;
 
+import java.util.WeakHashMap;
 import java.util.concurrent.atomic.AtomicLong;
 
 public class Main {
     public static void main(String[] args) {
-        Button button = new Button("버튼");
-        button.addListener(new IButtonListener() {
-            @Override
-            public void clickEvent(String event) {
-                System.out.println(event);
-            }
-        });
-        button.click("메시지 전달 : click 1");
-        button.click("메시지 전달 : click 2");
-        button.click("메시지 전달 : click 3");
-        button.click("메시지 전달 : click 4");
-        button.click("메시지 전달 : click 5");
+        Ftp ftpClient = new Ftp("www.naver.com", 22, "/home/etc");
+        ftpClient.connect();
+        ftpClient.moveDirectory();
+
+
+        Writer writer = new Writer("text.tmp");
+        writer.fileConnect();
+        writer.write();
+
+        Reader reader = new Reader("text.tmp");
+        reader.fileConnect();
+        reader.fileRead();
+
+        reader.fileDisconnect();
+        writer.fileDisconnect();
+        ftpClient.disConnect();
+
+        SftpClient sftpClient = new SftpClient("www.naver.com", 22, "/home/etc","text.tmp");
+        sftpClient.connect();
+        sftpClient.write();
+        sftpClient.read();
+        sftpClient.disConnect();
+
 
     }
 }
