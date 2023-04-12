eda937b7f7dbf6edbc36da2f13921f056e2cf1be
Joyanggi <jyg8033@gmail.com>
2023-04-12 09:25:56 +0900

    Adapter

--- src/main/java/com/company/design/Main.java
+++ src/main/java/com/company/design/Main.java
@@ -1,12 +1,13 @@
 package com.company.design;
 
+import com.company.design.adapter.*;
 import com.company.design.singleton.AClazz;
 import com.company.design.singleton.BClazz;
 import com.company.design.singleton.SocketClient;
 
 public class Main {
     public static void main(String[] args) {
-
+        /*
         AClazz aClazz = new AClazz();
         BClazz bClazz = new BClazz();
 
@@ -15,5 +16,22 @@ public class Main {
 
         System.out.println("두개의 객체가 동일한가?");
         System.out.println(aClient.equals(bClient));
+        */
+        HairDryer hairDryer = new HairDryer();
+        connect(hairDryer);
+
+        Cleaner cleaner = new Cleaner();
+
+        Electronic110V adapter = new SocketAdapter(cleaner);
+        connect(adapter);
+
+        AirConditioner airConditioner = new AirConditioner();
+        Electronic110V airAdapter = new SocketAdapter(airConditioner);
+        connect(airAdapter);
+
+    }
+    //콘센트
+    public static void connect(Electronic110V electronic110V){
+        electronic110V.powerOn();
     }
 }
